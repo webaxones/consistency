f73c5a1267fe92f8e5601d74159ee37fc23359d5
Loïc ANTIGNAC <contact@webaxones.com>
2024-05-10 15:48:43 +0200

    Fix vendor for class autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e903ea650a816f0941d87bebffc94c2618407e9f',
+        'name' => 'webaxones/consistency',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'webaxones/consistency' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e903ea650a816f0941d87bebffc94c2618407e9f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
