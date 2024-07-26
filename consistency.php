fa41d1d1056d7b7e666dff0f55870da2196804ed
Loïc ANTIGNAC <contact@webaxones.com>
2024-07-26 11:38:13 +0200

    fix version number that wasn't saved in main php file

--- consistency.php
+++ consistency.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Consistency
  * Plugin URI:        https://www.webaxones.com
  * Description:       Fixes typographical consistency
- * Version:           1.6.3
+ * Version:           1.6.4
  * Requires at least: 6.1
  * Requires PHP:      7.4
  * Author:            Loïc Antignac
